ae0601fc76ff92efa6839e01b2b79f437a1589e2
Артур Роднов <artur03rodnov@gmail.com>
2024-04-08 17:00:45 +0600

    Logo

--- src/Modules/Auth/SignIn/SignIn.tsx
+++ src/Modules/Auth/SignIn/SignIn.tsx
@@ -1,9 +1,10 @@
-import { Flex } from 'antd'
+import { Flex, Typography } from 'antd'
 import { LoginForm } from '../Components/LoginForm/LoginForm'
 
 export function SignIn() {
     return (
         <Flex vertical justify="center" align="center" style={{ width: '100vw', minHeight: '100vh' }}>
+            <Typography style={{fontSize: 30}}>KGN Logistics</Typography>
             <LoginForm />
         </Flex>
     )
--- src/Modules/Auth/SignUp/SignUp.tsx
+++ src/Modules/Auth/SignUp/SignUp.tsx
@@ -1,9 +1,10 @@
-import { Flex } from 'antd'
+import { Flex, Typography } from 'antd'
 import { SignUpForm } from '../Components/SignUpForm/SignUpForm'
 
 export function SignUp() {
     return (
         <Flex vertical justify="center" align="center" style={{ width: '100vw', minHeight: '100vh' }}>
+            <Typography style={{fontSize: 30}}>KGN Logistics</Typography>
             <SignUpForm />
         </Flex>
     )
